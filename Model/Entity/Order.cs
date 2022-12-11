d5fe063d000ccd1096e7c12ec0d4fba7c9880a83
NikolayCheremnov <coolnick1999@gmail.com>
2022-12-12 01:10:10 +0700

    Добавьте файлы проекта.

---
+++ Model/Entity/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudShopApp.Model.Entity
+{
+    // класс "Заказ в магазине"
+    public class Order
+    {
+        public int Id { get; set; }
+        public string FeedbackContact { get; set; } // данные обратной связи клиента
+        public string Description { get; set; }     // описание заказа
+
+        public override string ToString()
+        {
+            return $"{Id} - {FeedbackContact} - {Description}";
+        }
+    }
+}
---
+++ Service/IOrderServicecs.cs
@@ -0,0 +1,15 @@
+﻿using CloudShopApp.Model.Entity;
+
+namespace CloudShopApp.Service
+{
+    // интерфейс сервиса
+    public interface IOrderServicecs
+    {
+        // CRUD-интерфейс для заказа
+        List<Order> GetAllOrders();
+        Order GetOrderById(int id);
+        Order AddOrder(Order order);
+        void RemoveOrderById(int id);
+        void UpdateOrder(Order order);
+    }
+}
